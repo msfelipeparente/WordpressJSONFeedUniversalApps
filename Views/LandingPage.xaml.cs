e9ad3970d14c9fe47b833a22466d7a7c3a0161e0
msfelipeparente <felipedasilva65@gmail.com>
2016-02-05 21:42:52 -0200

    Update LandingPage.xaml.cs

--- Views/LandingPage.xaml.cs
+++ Views/LandingPage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
+using NavigationMenu.ViewModels;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -20,9 +25,49 @@ namespace NavigationMenuSample.Views
     /// </summary>
     public sealed partial class LandingPage : Page
     {
+        private ObservableCollection<ItemViewModel> Items;
+
         public LandingPage()
         {
             this.InitializeComponent();
+
+            Items = new ObservableCollection<ItemViewModel>();
+
+            LoadData();
+            /*
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+               
+            }*/
+        }
+
+
+        public async void LoadData()
+        {
+            Uri geturi = new Uri("http://tnwindows.com.br/api/get_recent_posts/");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responseGet = await client.GetAsync(geturi);
+            string response = await responseGet.Content.ReadAsStringAsync();
+            Posts post1 = JsonConvert.DeserializeObject<Posts>(response);
+            if (post1.posts != null && post1.posts.Count > 0)
+            {
+                foreach (Post post in post1.posts)
+                {
+                    string date = post.date;
+                    DateTime dt = Convert.ToDateTime(date);
+                    string dtString = dt.ToString("dd MMM yyyy HH:mm:ss").ToUpper();
+
+                    int dateMax = 12;
+
+                    dtString = dtString.ToString().Substring(0, dateMax);
+                    this.Items.Add(new ItemViewModel() { LineOne = post.title, LineTwo = dtString, Thumbnail = post.thumbnail, ID = post.id });
+
+
+                }
+
+            }
         }
     }
 }
+
